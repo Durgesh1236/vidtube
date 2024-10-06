4a9d2a269b4626a0958dc97c08c4c1d5214231b2
Durgesh kumar <Kumardurgeshjha398@gmail.com>
2024-10-06 10:45:40 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Durgesh1236/vidtube.git',
+  base: 'https://github.com/Durgesh1236/vidtube/',
 })
